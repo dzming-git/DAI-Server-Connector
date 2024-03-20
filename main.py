e8d154b62c72929916822d8ca1f0743c63fe53bf
dzming <dzm_work@163.com>
2024-03-20 12:34:25 +0000

    该服务作为网关，不需要向consul注册；并更新了port为int类型

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 from src.config.config import Config
-import time
 from src.app import app
 
 config = Config()
 
-
 def connect_consul():
     from src.consul import ConsulClient
-    from src.consul import ServiceInfo
-    from src.utils import get_local_ip_address
-
     consul_client = ConsulClient()
     consul_client.consul_ip = config.consul_ip
     consul_client.consul_port = config.consul_port
-    host = get_local_ip_address()
-    service_name = config.service_name
-    service_port = config.service_port
-    service_info = ServiceInfo()
-    service_info.service_ip = host
-    service_info.service_port = service_port
-    service_info.service_name = service_name
-    service_info.service_id = f'{service_name}-{host}-{service_port}'
-    service_info.service_tags = config.service_tags
-    consul_client.register_service(service_info)
 
 if __name__ == '__main__':
     connect_consul()
